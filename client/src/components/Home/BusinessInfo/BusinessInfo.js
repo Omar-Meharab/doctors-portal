66df193d3eb097b1cf8cd63f167b34ad0cc65f8f
Omar Meharab <omarmeharab81@gmail.com>
2021-04-14 14:07:23 +0600

    a

--- client/src/components/Home/BusinessInfo/BusinessInfo.js
+++ client/src/components/Home/BusinessInfo/BusinessInfo.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import InfoCard from '../InfoCard/InfoCard';
+import { faClock, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons'
+
 const infosData = [
     {
         title: 'Opening Hours',
         description: 'We are open 24/7',
-        icon: '',
+        icon: faClock,
         background: 'primary'
     },
     {
         title: 'Visit Our Location',
         description: 'Brooklyn, NY 10003 USA',
-        icon: '',
+        icon: faMapMarker,
         background: 'dark'
     },
     {
         title: 'Call Us Now',
         description: '+15697854124',
-        icon: '',
+        icon: faPhone,
         background: 'primary'
     }
 ]
 
 const BusinessInfo = () => {
     return (
-        <section className="row">
-            {
-                infosData.map(info => <InfoCard info={info}></InfoCard>)
-            }
+        <section className="d-flex justify-content-center">
+            <div className="w-75 row">
+                {
+                    infosData.map(info => <InfoCard info={info}></InfoCard>)
+                }
+            </div>
         </section>
     );
 };
